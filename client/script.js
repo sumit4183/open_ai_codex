9780fb96009eadb0c4c793ac5621eae1080c1b7e
SUMIT <sumit4183@yahoo.com>
2022-12-30 12:29:09 +0530

    Update server url

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
   // messageDiv.innerHTML = "..."
   loader(messageDiv);
 
-  const response = await fetch('https://codex-csui.onrender.com', {
+  const response = await fetch('https://codex-7ert.onrender.com', {
     method: 'POST',
       headers: {
         'Content-Type': 'application/json',
